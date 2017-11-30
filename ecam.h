a9aacf5941d8df1d60c63e67f3b7c6821b8a5441
Eric Holland <eric.holland@gmail.com>
2017-11-30 05:48:51 -0800

    assorted cleanup

--- ecam.h
+++ ecam.h
@@ -7,43 +7,46 @@ using namespace cv;
 
 class eCamera {
 public:
-	static eCamera *Create();
-	eCamera();
-	eCamera(uint32_t idx);
-	~eCamera();
-	bool 		isConnected() {return connected_;};
+    static eCamera *Create();
+    static uint32_t NumConnectedCameras();
 
-	long 		getGain() {return getVal(ASI_GAIN);};
-	long 		setGain(long val) { return setVal(ASI_GAIN,val);};
-	long		getExposure() { return getVal(ASI_EXPOSURE);};
-	long 		setExposure(long val) { return setVal(ASI_EXPOSURE,val);};
-	float 		getTemperature() {return (float)getVal(ASI_TEMPERATURE)/10.0;}
-	void 		startExposure();
-	int 		exposureStatus();
-	uint32_t 	getWidth() { return width_;};
-	uint32_t 	getHeight() { return height_;};
-	uint32_t 	setMonoBin() { return setVal(ASI_MONO_BIN,1);};
-	long		isHWBin() { return getVal(ASI_HARDWARE_BIN);};
-	uint32_t 	loadData();
-	uint32_t 	showData();
-	double	adu();
-	void showit();
+    eCamera();
+    eCamera(uint32_t idx);
 
-	static uint32_t NumConnectedCameras();
+    ~eCamera();
+
+    long        getGain()               { return getVal(ASI_GAIN);};
+    long        setGain(long val)       { return setVal(ASI_GAIN,val);};
+    long        getExposure()           { return getVal(ASI_EXPOSURE);};
+    long        setExposure(long val)   { return setVal(ASI_EXPOSURE,val);};
+    float       getTemperature()        { return (float)getVal(ASI_TEMPERATURE)/10.0;}
+    uint32_t    setMonoBin()            { return setVal(ASI_MONO_BIN,1);};
+    long        isHWBin()               { return getVal(ASI_HARDWARE_BIN);};
+
+    void        startExposure();
+    int         exposureStatus();
+
+    uint32_t    getWidth()              { return width_;};
+    uint32_t    getHeight()             { return height_;};
+    bool        isConnected()           { return connected_;};
+
+    uint32_t    loadData();
+    uint32_t    showData();
+    double      adu();
+    void        showit();
+
 
 private:
-	long getVal( ASI_CONTROL_TYPE ctl);
-	long setVal( ASI_CONTROL_TYPE ctl, long val);
-	bool connected_;
-	int idx_ = -1;
-	uint32_t width_;
-	uint32_t height_;
-	uint32_t pixel_size_;
-	uint8_t *buf_ = NULL;
-	Mat image_;
-	Mat im_rgb16_;
-	Mat im_rgb_;
-	Mat im_preview_;
-
-
+    long getVal( ASI_CONTROL_TYPE ctl);
+    long setVal( ASI_CONTROL_TYPE ctl, long val);
+    bool connected_;
+    int idx_ = -1;
+    uint32_t width_;
+    uint32_t height_;
+    uint32_t pixel_size_;
+    uint8_t *buf_ = NULL;
+    Mat image_;
+    Mat im_rgb16_;
+    Mat im_rgb_;
+    Mat im_preview_;
 };
